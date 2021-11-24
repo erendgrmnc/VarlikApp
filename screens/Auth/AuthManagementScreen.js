8def48193c126d6e9b19362c54483e35217a3dff
YasinUnall <yasinunalnn@gmail.com>
2021-11-24 22:39:42 +0300

    Auth bug fixed.

--- screens/Auth/AuthManagementScreen.js
+++ screens/Auth/AuthManagementScreen.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignUpScreen from './SignUpScreen';
-import SingInScreen from './SingInScreen';
-import { auth } from '../../firebase';
-import WalletScreen from '../Wallet/WalletScreen';
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignUpScreen from "./SignUpScreen";
+import SingInScreen from "./SingInScreen";
+import { auth } from "../../firebase";
+import WalletScreen from "../Wallet/WalletScreen";
 const AuthManagementScreen = (prop, { navigation }) => {
+  function GetSignUpScreen() {
+    return <SignUpScreen />;
+  }
 
+  function GetSignInScreen() {
+    return <SingInScreen />;
+  }
 
-    function GetSignUpScreen() {
-        return <SignUpScreen />
+  function GetWalletScreen() {
+    return <WalletScreen />;
+  }
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      navigation.navigate("Wallet");
+    } else {
+      navigation.navigate("Sign In");
     }
+  });
 
-    function GetSignInScreen() {
-        return <SingInScreen />
-    }
+  const Stack = createNativeStackNavigator();
 
-    function GetWalletScreen() {
-        return <WalletScreen />
-    }
-
-    auth().onAuthStateChanged((user) => {
-        if (user) {
-            navigation.navigate('Wallet');
-        }
-        else {
-            navigation.navigate('Sign In');
-        }
-    });
-
-    const Stack = createNativeStackNavigator();
-
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name='Sign In' component={GetSignInScreen} />
-            <Stack.Screen name='Wallet' component={GetWalletScreen} />
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Sign In" component={GetSignInScreen} />
+      <Stack.Screen name="Wallet" component={GetWalletScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default AuthManagementScreen;
