b20bd909fed4bed30efc5c83af998dfd26943d13
YasinUnall <yasinunalnn@gmail.com>
2021-12-01 21:47:49 +0300

    Style changes were made.

--- components/Stock/StockContainer.js
+++ components/Stock/StockContainer.js
@@ -1,69 +1,59 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
-const GetPercentageText = (percent) => {
-    if (percent > 0) {
-        return (
-            <Text style={styles.positivePercentText}>{percent}</Text>
-        );
-    }
-    else if (percent < 0) {
-        return (
-            <Text style={styles.negativePercentText}>{percent}</Text>
-        );
-    }
-    else {
-        return (
-            <Text>{percent}</Text>
-        );
-    }
-}
+const GetPercentageText = (percent, priceStyle) => {
+  if (percent > 0) {
+    return (
+      <Text style={[styles.positivePercentText, priceStyle]}>{percent}</Text>
+    );
+  } else if (percent < 0) {
+    return (
+      <Text style={[styles.negativePercentText, priceStyle]}>{percent}</Text>
+    );
+  } else {
+    return <Text>{percent}</Text>;
+  }
+};
 
 const StockContainer = (prop) => {
-    return (
-        <View style={styles.mainContainer}>
-            <View>
-                <Text>{prop.stock.text}({prop.stock.code})</Text>
-            </View>
-            <View style={styles.pricesContainer}>
-                <View>
-                    <Text>
-                        {prop.stock.lastprice} ₺
-                    </Text>
-                </View>
-                <View>
-                    {GetPercentageText(prop.stock.rate)}
-                </View>
-
-
-            </View>
+  return (
+    <View style={styles.mainContainer}>
+      <View>
+        <Text style={prop.mainTextStyle}>
+          {prop.stock.text}({prop.stock.code})
+        </Text>
+      </View>
+      <View style={styles.pricesContainer}>
+        <View>
+          <Text style={prop.pricesTextStyle}>{prop.stock.lastprice} ₺</Text>
         </View>
-    )
+        <View>{GetPercentageText(prop.stock.rate, prop.pricesTextStyle)}</View>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        width: '100%',
-        alignItems: 'flex-start',
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        marginVertical: 5,
-        paddingVertical: 5,
-        paddingHorizontal: 3
-
-
-    },
-    positivePercentText: {
-        color: 'green'
-    },
-    negativePercentText: {
-        color: 'red'
-    },
-    pricesContainer: {
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end'
-    }
+  mainContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginVertical: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 3,
+  },
+  positivePercentText: {
+    color: "green",
+  },
+  negativePercentText: {
+    color: "red",
+  },
+  pricesContainer: {
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
 });
 
 export default StockContainer;
--- components/Stock/StockScrollView.js
+++ components/Stock/StockScrollView.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
-import StockContainer from './StockContainer';
-
+import React from "react";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
+import StockContainer from "./StockContainer";
 
 const StockScrollView = (prop) => {
-    const listStocks = prop.stockList.map((stockObj, index) => {
-        return (
-            <View key={index}>
-                <StockContainer stock={stockObj.stock} />
-            </View>
-        )
-    })
-
+  const listStocks = prop.stockList.map((stockObj, index) => {
     return (
-        <ScrollView>
-            {listStocks}
-        </ScrollView>
+      <View key={index}>
+        <StockContainer
+          mainTextStyle={prop.mainTextStyle}
+          pricesTextStyle={prop.pricesTextStyle}
+          stock={stockObj.stock}
+        />
+      </View>
     );
-}
+  });
+
+  return <ScrollView style={prop.style}>{listStocks}</ScrollView>;
+};
 
 const styles = StyleSheet.create({
-
+  mainTextStyle: {
+    fontSize: 13,
+  },
+  pricesTextStyle: {
+    fontSize: 12,
+  },
 });
 
 export default StockScrollView;
